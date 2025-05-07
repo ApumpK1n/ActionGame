735b578e2817f29fe19a532183db76e63d577771
lis <30541379+ApumpK1n@users.noreply.github.com>
2025-05-07 20:54:02 +0800

    增加剑气

--- Assets/ActionGame/Scripts/Config/ProjectileAbilityConfig.cs
+++ Assets/ActionGame/Scripts/Config/ProjectileAbilityConfig.cs
@@ -3,6 +3,7 @@
 using CombatAbilitySystem;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "CombatAbilitySystem")]
 public class ProjectileAbilityConfig : AbilityConfig
 {
     [SerializeField] protected Projectile projectile;
