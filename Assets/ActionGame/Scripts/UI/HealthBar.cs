aea3330b8f5dd341dd1e87054cd3ff0ed15a6d6e
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2025-08-08 00:31:39 +0800

    增加血条

---
+++ Assets/ActionGame/Scripts/UI/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private SlicedFilledImage healthProgress;
+    [SerializeField] private TextMeshProUGUI textHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
